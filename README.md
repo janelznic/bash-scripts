9fcb9ca29be71c3a153cbe8dc48ee2603e09f048
Jan Elznic <jan@elznic.com>
2019-09-17 15:21:24 +0200

    User configuration and README file added

---
+++ README.md
@@ -0,0 +1,22 @@
+bash-scripts
+------------
+* Bash scripts for Debian
+* Maintainer: [Jan Elznic](https://janelznic.cz), <jan@elznic.com>
+
+
+## config-user.sh
+* Set up basic user configuration for SSH, Vim, Bash (aliases), Git & NPM
+```bash
+curl -sL https://raw.githubusercontent.com/janelznic/bash-scripts/master/bin/sshfsall.sh | bash -
+```
+
+
+## mountall.sh
+* Mount remote connection via Samba from file `data/mount_servers.dat` to `/home/<user>/mnt/<user@server>`
+
+
+## sshfsall.sh
+* Mount remote connection via SSHFS from file `data/sshfs_servers.dat` to `/home/<user>/mnt/<user@server>`
+
+
+MIT License Copyright (c) 2016-2019 Jan Elznic
---
+++ bin/config-user.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+# Reading input variables
+FULLNAME=$(getent passwd $USER | cut -d ':' -f 5 | cut -d ',' -f 1)
+read -p "Your e-mail: " EMAIL
+
+# SSH
+mkdir ~/.ssh
+touch ~/.ssh/authorized_keys
+echo "You can edit your public keys on: ~/.ssh/authorized_keys"
+
+# Vim
+echo "syntax on" >> .vimrc
+echo "set mouse-=a" >> .vimrc
+
+# Bash aliases
+echo "alias ll='ls -la'" >> .bashrc
+echo "alias e='exit'" >> .bashrc
+
+# Git
+git config --global user.name $FULLNAME
+git config --global user.email $EMAIL
+git config --global color.ui auto
+
+git config --global alias.co checkout
+git config --global alias.br branch
+git config --global alias.ci commit
+git config --global alias.st status
+git config --global alias.unstage 'reset HEAD --'
+git config --global alias.last 'log -1 HEAD'
+
+# NodeJS
+mkdir ~/.npm-global
+npm config set prefix '~/.npm-global'
+echo "export PATH=~/.npm-global/bin:\$PATH" >> ~/.profile
+source ~/.profile
+
+# Status
+echo "Done."
